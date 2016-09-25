1737fdbd00ffa2141d276b144561708e8c5833f5
Aswin Pillai <aswing05@gmail.com>
2016-09-26 01:23:15 +0530

    scropt updated

--- run_analysis.R
+++ run_analysis.R
@@ -1,12 +1,12 @@
 #Step 1: Merge the training and test data sets
 
-x_train <- read.table("UCI HAR Dataset/train/X_train.txt")
-y_train <- read.table("UCI HAR Dataset/train/y_train.txt")
-subject_train <- read.table("UCI HAR Dataset/train/subject_train.txt")
+x_train <- read.table("train/X_train.txt")
+y_train <- read.table("train/y_train.txt")
+subject_train <- read.table("train/subject_train.txt")
 
-x_test <- read.table("UCI HAR Dataset/test/X_test.txt")
-y_test <- read.table("UCI HAR Dataset/test/y_test.txt")
-subject_test <- read.table("UCI HAR Dataset/test/subject_test.txt")
+x_test <- read.table("test/X_test.txt")
+y_test <- read.table("test/y_test.txt")
+subject_test <- read.table("test/subject_test.txt")
 
 #create the merged datasets
 xdata <- rbind(x_train, x_test)
@@ -17,7 +17,7 @@ subjectdata <- rbind(subject_train, subject_test)
 
 #Step 2: Extracts only the measurements on the mean and standard deviation for each measurement
 
-features <- read.table("UCI HAR Dataset/features.txt")
+features <- read.table("features.txt")
 
 mean_std_features <- grep("-(mean|std)\\(\\)", features[,2])
 
@@ -27,7 +27,7 @@ names(xdata) <- features[mean_std_features,2]
 
 #Step 3: Uses descriptive activity names to name the activities in the data set
 
-activity_labels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activity_labels <- read.table("activity_labels.txt")
 
 ydata[,1] <- activity_labels[ydata[,1],2]
 
